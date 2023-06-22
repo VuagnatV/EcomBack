a9496c3e8ae5561dd6f0ae9f938b6d438fe012ef
VuagnatV <37271745+VuagnatV@users.noreply.github.com>
2023-06-22 15:33:04 +0200

    f

--- src/middlewares/isAdmin.js
+++ src/middlewares/isAdmin.js
@@ -1,14 +1,20 @@
 
 const isAdmin = (req, res, next) => {
     // Check if user is authenticated (logged in)
-    if (req.session.user.role === "admin") {
-        // User is logged in
-        console.log("admin checked")
-        next(); // Proceed to the next middleware or route handler
+    if (req.session.user) {
+        if (req.session.user.role === "admin") {
+            // User is logged in
+            console.log("admin checked")
+            next(); // Proceed to the next middleware or route handler
+        } else {
+            // User is not logged in
+            res.status(401).send('Unauthorized');
+        }
     } else {
         // User is not logged in
         res.status(401).send('Unauthorized');
     }
+
 };
 
 module.exports = isAdmin;
